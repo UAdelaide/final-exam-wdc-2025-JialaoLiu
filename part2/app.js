dc998e86ac87886487ec4f3f10d4725c5ad34d5c
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-06-20 15:04:54 +0000

    [automatic commit] 2025-06-20 15:04:53 UTC 58.174.112.100

--- part2/app.js
+++ part2/app.js
@@ -44,7 +44,9 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
+app.get('/owner-dashboard', function(req, res) {
+  if (!req.session.user || req.session.user.role !== 'owner') {
+    return res.status(403).send('Access denied');
 
 
 
