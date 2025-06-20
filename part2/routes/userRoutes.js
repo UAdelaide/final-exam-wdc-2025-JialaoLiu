9221007f694cb10e750f9503864c171edfb90d05
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-06-20 16:57:55 +0000

    [automatic commit] 2025-06-20 16:57:55 UTC 58.174.112.100

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,7 +35,7 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST login (dummy version)
+// POST login (clever version)
 router.post('/login', async (req, res) => {
   // const { email, password } = req.body;
   const { username, password } = req.body;
