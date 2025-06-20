6d6af4165a9a220edefc73308eb987ad4fce3b99
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-06-20 16:23:36 +0000

    [automatic commit] 2025-06-20 16:23:36 UTC 58.174.112.100

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -97,6 +97,7 @@ router.post('/logout', (req, res) => {
     if (err) {
       return res.status(500).json({ error: 'Logout failed' });
     }
+    res.clearCookie('connect.sid'); // Clear the session cookie
     res.json({ message: 'Logged out successful' });
   });
 });
