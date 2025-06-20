08fba2a1c7972f9e951fad432674e0ac98e81e15
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-06-20 09:18:17 +0000

    [automatic commit] 2025-06-20 09:18:17 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -81,7 +81,7 @@ app.get('/api/dogs', async (req, res) => {
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
         ORDER BY d.name;
-    `
+    `;
         const [results] = await promisePool.query(query);
         res.json(results);
     } catch (error) {
