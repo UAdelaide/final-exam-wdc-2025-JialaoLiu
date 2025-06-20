712f48bc5b4654f0535837fd9a750a31950d7c35
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-06-20 15:03:52 +0000

    [automatic commit] 2025-06-20 15:03:52 UTC 58.174.112.100

--- part2/app.js
+++ part2/app.js
@@ -33,12 +33,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-app.use('/walks', walkRoutes);
-app.use('/users', userRoutes);
+app.use()
 
 
 
