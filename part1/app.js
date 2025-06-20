3a1b94f7e1f34914bd178cd6b5ea8b2ee4e39207
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-06-20 09:13:11 +0000

    [automatic commit] 2025-06-20 09:13:11 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -86,4 +86,6 @@ app.get('/api/dogs', async (req, res) => {
         res.json(results);
     } catch (error) {
         console.error('Database error:', error);
-        res.status(500).json(error): ''})
+        res.status(500).json(error): 'Fetching dogs failed'});
+    }
+});
