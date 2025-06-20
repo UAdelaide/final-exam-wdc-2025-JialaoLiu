d9050abdf6b83659dc17350b76e5adfd15b99523
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-06-20 09:34:39 +0000

    [automatic commit] 2025-06-20 09:34:39 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -95,18 +95,18 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const query = `
-        SELECT
-        wr.request_id,
-        d.name as dog_name,
-        wr.requested_time,
-        wr.duration_minutes,
-        wr.location,
-        u.username as owner_username,
-    FROM WalkRequests wr
-        JOIN Dogs d ON wr.dog_id = d.dog_id
-        JOIN Users u ON d.owner_id = u.user_id
-        WHERE wr.status = 'open'
-        ORDER BY wr.requested_time;
+            SELECT
+                wr.request_id,
+                d.name as dog_name,
+                wr.requested_time,
+                wr.duration_minutes,
+                wr.location,
+                u.username as owner_username
+            FROM WalkRequests wr
+            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON d.owner_id = u.user_id
+            WHERE wr.status = 'open'
+            ORDER BY wr.requested_time
     `;
         const [results] = await promisePool.query(query);
         res.json(results);
