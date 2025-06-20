47e6a6068f0564959a5f61f7690f3c00aa04d064
JialaoLiu <Jarvis_Ishiyari@outlook.com>
2025-06-20 09:05:21 +0000

    [automatic commit] 2025-06-20 09:05:20 UTC 58.174.112.100

--- part1/app.js
+++ part1/app.js
@@ -50,6 +50,14 @@ async function initializeDatabase() {
   ('testwalker', 'testwalker@example.com', 'hashed999', 'walker');
         `);
         console.log('Users inserted')
-        await promisePool.execute(`
+        await promisePool.execute(`INSERT INTO Dogs (name, size, owner_id)
+VALUES
+  ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+  ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
+  ('Cookie', 'large', (SELECT user_id FROM Users WHERE username = 'Jarvis')),
+  ('Rookie', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+  ('Trey', 'small', (SELECT user_id FROM Users WHERE username = 'Jarvis'));
             `);
+
+        await
     }
